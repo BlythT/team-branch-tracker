9a3f5dc43d707c6710fe8f963b1d94d4535171a5
Tim Blyth <timmblyth@gmail.com>
2022-09-03 18:12:23 +1200

    Assign teams and people IDs and pass as state

--- src/components/TeamTracker.vue
+++ src/components/TeamTracker.vue
@@ -1,27 +1,47 @@
+<!-- eslint-disable prettier/prettier -->
 <script>
 import config from "../../config.json";
 
 export default {
   data() {
-    let people = [];
-    let teams = [];
-    if (Object.prototype.hasOwnProperty.call(config, "people")) {
-      config.people.forEach((personData, index) => {
-        people.push({ id: index, name: personData.name });
-      });
-    }
+    let unassigned = { teamid: 0, people: [] };
 
-    if (Object.prototype.hasOwnProperty.call(config, "teams")) {
-      config.teams.forEach((branch) => {
-        teams.push({ name: branch.name });
-      });
+    // Remove any invalid teams from state
+    const validTeams = config.teams.map((team) => team.id);
+    config.state.forEach((team, index) => {
+      if (!validTeams.includes(team.teamid)) {
+        config.state.splice(index, 1); // Remove invalid team from state now that people have been moved
+      }
+    });
 
-      return {
-        people: people,
-        branches: teams,
-        dragItem: null,
-      };
-    }
+    // Add any missing teams from state
+    const stateTeams = config.state.map((team) => team.teamid)
+    const missingTeams = validTeams.filter((teamid) => !stateTeams.includes(teamid))
+    missingTeams.forEach((teamid) => {
+      config.state.push({"teamid": teamid, people: []})
+    })
+
+    // If people config contains a person not in state, assign to unnasigned
+    let assignedUsers = [];
+    config.state.forEach((team) => {
+      assignedUsers = assignedUsers.concat(team.people);
+    });
+
+    config.people.forEach((person) => {
+      if (!assignedUsers.includes(person.id)) {
+        unassigned.people.push(person.id);
+      }
+    });
+
+    config.state.unshift(unassigned);
+    console.log(config.state);
+
+    return {
+      people: config.people,
+      teams: config.teams,
+      dragItem: null,
+      state: config.state
+    };
   },
   methods: {
     dragStart(e) {
@@ -38,6 +58,22 @@ export default {
         e.target.append(this.dragItem);
       }
     },
+
+    getTeam(teamid) {
+      const out = this.teams.filter(team => team.id === teamid)[0]
+      if (out) {
+        return out
+      }
+      return {id: teamid, name: "unknown team", description: "unknown team"}
+    },
+
+    getPerson(personid) {
+      const out = this.people.filter(person => person.id === personid)[0]
+      if (out) {
+        return out
+      }
+      return {id: personid, name: "unkown person", description: "unkown person"}
+    }
 
     // Unused but could be used for styling on enter and exit (shimmer/throb/colour-shift effect)
     // dragEnter() {
@@ -58,27 +94,24 @@ export default {
 <template>
   <main>
     <div class="container">
-      <ul class="column default-column" @dragover.prevent @drop="dragDrop">
-        <h1>Unassigned</h1>
-        <li
-          class="item"
-          draggable="true"
-          v-for="person in people"
-          :key="person.id"
-          @dragstart="dragStart"
-          @dragend="dragEnd"
-        >
-          {{ person.name }}
-        </li>
-      </ul>
       <ul
         class="column"
-        v-for="branch in branches"
-        :key="branch.name"
+        v-for="team in state"
+        :key="team.teamid"
         @dragover.prevent
         @drop.self="dragDrop"
       >
-        <h1>{{ branch.name }}</h1>
+        <h1>{{ getTeam(team.teamid).name }}</h1>
+        <li
+          class="item"
+          draggable="true"
+          v-for="personid in team.people"
+          :key="personid"
+          @dragstart="dragStart"
+          @dragend="dragEnd"
+        >
+          {{ personid }}
+        </li>
       </ul>
     </div>
   </main>
