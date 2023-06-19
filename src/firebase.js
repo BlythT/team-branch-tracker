2844fc84e9c145e833de677d16d88fc8964bd56e
Tim Blyth <timmblyth@gmail.com>
2023-06-20 09:01:45 +1200

    convert creds to environment variables and roll them, since they're in git history
    This is a more secure way of storing them, though
    they are bundled with the vite source when running
    vite build so I will have to look into whether
    that exposes them in a different way

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getDatabase } from 'firebase/database';
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCGyx6IpPkWC60U65-aWQEZCTYC_P43fOM",
-	authDomain: "team-tracker-74588.firebaseapp.com",
-	databaseURL: "https://team-tracker-74588-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "team-tracker-74588",
-	storageBucket: "team-tracker-74588.appspot.com",
-	messagingSenderId: "676267504915",
-	appId: "1:676267504915:web:35e58cccb071a06b1156e6"
+	apiKey: import.meta.env.VITE_API_KEY,
+	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+	databaseURL: import.meta.env.VITE_DATABASE_URL,
+	projectId: import.meta.env.VITE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
